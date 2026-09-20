e550ee964769eb074d7a5c9504def2fc30ed48fc
atoliulla32 <atoliulla@gmail.com>
2026-09-20 08:11:23 +0600

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,15 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // প্রতিবার নতুন লিংক তৈরি করবে, ফলে ব্রাউজার ক্যাশ ধরে রাখতে পারবে না
+    const noCache = "?v=" + new Date().getTime();
+
     // হেডার লোড করা
-    fetch('header.html')
+    fetch('header.html' + noCache, { cache: "no-store" })
         .then(response => response.text())
         .then(data => {
             document.getElementById('header-placeholder').innerHTML = data;
         });
 
     // ফুটার লোড করা
-    fetch('footer.html')
+    fetch('footer.html' + noCache, { cache: "no-store" })
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer-placeholder').innerHTML = data;
         });
-});w
+});
